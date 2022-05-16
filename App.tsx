b01b4772c3d63138c5b80fd28022d49965d60100
88pawankumarjha <88pawankumarjha@gmail.com>
2022-05-16 23:47:30 +0530

    dark theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,49 @@
 import * as React from 'react';
+import data from './assets/data';
+import DarkMode from './components/DarkMode';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {/* https://github.com/recharts/recharts */}
+      {/* https://dev.to/alexeagleson/how-to-create-a-dark-mode-component-in-react-3ibg */}
+      <div className="visualization">
+        <DarkMode />
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </div>
     </div>
   );
 }
